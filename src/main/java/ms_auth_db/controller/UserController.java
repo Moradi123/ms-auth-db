d563b4aca04c6b9330525399f57fa425b0f2a98a
Moradia123 <jo.morad@duocuc.cl>
2025-07-06 18:20:41 -0400

    Se añade el Swagger

--- src/main/java/ms_auth_db/controller/UserController.java
+++ src/main/java/ms_auth_db/controller/UserController.java
@@ -1,6 +1,29 @@
 package ms_auth_db.controller;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import ms_auth_db.model.entities.User;
+import ms_auth_db.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@Tag(name = "Usuarios - DB", description = "Operaciones directas a la base de datos de usuarios")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @Operation(summary = "Obtener todos los usuarios de la BD", description = "Devuelve una lista con todos los usuarios registrados en la tabla 'users'.")
+    @GetMapping("/users")
+    public List<User> selectAllUser(){
+        return userService.selectAllUser();
+    }
+}
+/*package ms_auth_db.controller;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +40,5 @@ public class UserController {
     public List<User> selectAllUser(){
         return userService.selectAllUser();
     }
-}
+}*/
 
